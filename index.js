b01250ec8f69e7a746bf17dd39cce398b5a335a6
Michael Rodriguez <michael.rodriguezg99@gmail.com>
2022-07-08 11:28:00 -0400

    Contact done for the index section

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+let isModalOpen = false;
+
+const scaleFactor = 1 / 20;
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_80ih0if",
+      "template_d9refyl",
+      event.target,
+      "user_K1PoFs8pB2YVWStDxrUls"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+      );
+    });
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
+
 function openMenu() {
   document.body.classList += " menu--open";
 }
@@ -5,4 +42,3 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove("menu--open");
 }
-
